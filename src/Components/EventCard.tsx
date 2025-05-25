533947105bdc48b2a14fb2f59d7201a927a706db
SHAHRIAR UTCHAS <123489314+Shahriar-Utchas@users.noreply.github.com>
2025-05-25 17:22:01 +0600

    basic structure built

---
+++ src/Components/EventCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EventCard = () => {
+    return (
+        <div>
+            <h1>Event card</h1>
+        </div>
+    );
+};
+
+export default EventCard;
---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Navbar from "@/Components/Navbar";
+import Footer from "@/Components/Footer";
+
+export const metadata: Metadata = {
+  title: "EventBuddy",
+  description: "Event Booking System",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
